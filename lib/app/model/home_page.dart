ba58ab7024a223c3cd6aec60c24d815505cc9b48
AdrianoRAV <https://github.com/AdrianoRAV/site.git>
2021-12-09 10:54:30 -0300

    decoration

--- lib/app/model/home_page.dart
+++ lib/app/model/home_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -5,6 +7,29 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Container(
+            
+           decoration: BoxDecoration(
+              gradient: RadialGradient(
+                center: Alignment(-0.5,-0.6),
+                radius: 0.15,
+                // ignore: prefer_const_literals_to_create_immutables
+                colors: <Color>[
+                  Color(0xFFEEEEEE),
+                  Color(0xFF111133),
+                ],
+                stops:<double>[0.9,1.0],
+                 ),
+            ),
+            
+          )
+        ],
+      ),
+    );
   }
 }
+
+ 
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:imc_app/app/view/my_app.dart';
 
+
 void main() {
   runApp(const MyApp());
 }
