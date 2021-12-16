6de195a2691f8197ae4448c6f42eade2cee92660
baborges45 <brunoquemsoueu@gmail.com>
2021-12-16 10:11:51 -0300

    creating icon_content

---
+++ lib/app/components/icon_content.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:imc_app/app/model/constants.dart';
+
+class IconContent extends StatelessWidget {
+  final IconData icon;
+  final String label;
+
+  const IconContent({Key? key, required this.icon, required this.label})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          icon,
+          size: 80.0,
+          color: Colors.white,
+        ),
+        SizedBox(
+          height: 15.0,
+        ),
+        Text(
+          label,
+          style: kLabelTextStyle,
+        ),
+      ],
+    );
+  }
+}
